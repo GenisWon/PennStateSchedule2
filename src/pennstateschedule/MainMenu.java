674ac8afd6a6538320f993287c589860f2f788ec
cjd258 <cjd258@BKGAIGE205-13.win.psu.edu>
2018-03-15 14:37:56 -0400

    Initial commit

---
+++ src/pennstateschedule/MainMenu.java
@@ -0,0 +1,15 @@
+package pennstateschedule;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author cjd258
+ */
+public interface MainMenu {
+    
+}
---
+++ src/pennstateschedule/PennStateSchedule.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pennstateschedule;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author cjd258
+ */
+public class PennStateSchedule {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JFrame frame = new NewJFrame();
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    
+}
